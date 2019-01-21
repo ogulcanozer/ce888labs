5901f47bb947dfd823968a5f245d7221e338364c
Ozer <oo18830@essex.ac.uk>
2019-01-21 14:43:39 +0000

    vehicles scatter plot added

--- lab2/vehicles.py
+++ lab2/vehicles.py
@@ -4,8 +4,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
+#Read the .csv file
 df = pd.read_csv('./vehicles.csv')
 
+#Scatter plot of vehicles.csv
+splot = sns.lmplot(df.columns[0], df.columns[1], data=df, fit_reg=False)
+splot.axes[0,0].set_ylim(0,)
+splot.axes[0,0].set_xlim(0,)
+splot.savefig("scatterplot.png",bbox_inches='tight')
+splot.savefig("scatterplot.pdf",bbox_inches='tight')
+
+plt.clf()
+
+#Histogram of the current fleet
 cf_plot = sns.distplot(df.iloc[:,0],kde=False, rug=True).get_figure()
 axes = plt.gca()
 axes.set_xlabel('Current Fleet No') 
@@ -15,6 +26,8 @@ cf_plot.savefig("cf_histogram.png",bbox_inches='tight')
 cf_plot.savefig("cf_histogram.pdf",bbox_inches='tight')
 
 plt.clf()
+
+#Histogram of the proposed fleet
 pf_plot = sns.distplot(df.iloc[:,1].dropna(), kde=False, rug=True).get_figure()
 axes = plt.gca()
 axes.set_xlabel('Proposed Fleet') 
